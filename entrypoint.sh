14628ae84de1982f851a14c5d464983a5d068ab0
Celenovich Klarulor <klarulor@gmail.com>
2022-06-05 21:04:49 +0300

    FixedReinstalling

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,35 +4,7 @@ reinstall=${REINSATLL_EXILED:-0}
 
 SERVER_PATH="/home/container/scp_server/"
 CONFIG_PATH="/home/container/.config"
-#DATA_PATH="/home/container/data"
-
-# Mount directories and copy origin files
-#mkdir home/container/data/SCPSL home/container/data/EXILED
-#mkdir home/container/data/SCPSL/config home/container/data/EXILED/Plugins home/container/data/EXILED/Configs home/container/data/EXILED/Logs
-#cp -r .$DATA_PATH/. .$CONFIG_PATH/
-
-if [ ! -f /installed ]; then 
-    if [ reinstall != 0] && [ modded == 1 ]; then 
-        reinstallExiled
-    fi
-    if [ modded == 0 ]; then
-        reinstallServer
-    fi
-    touch /installed #Configured
-fi
-
-
-
-
-# Startup server
-export DOTNET_BUNDLE_EXTRACT_BASE_DIR="temp"
-echo "Stating server at $server_port port.."
-cd $SERVER_PATH
-./LocalAdmin $server_port
-
-
-
-# if  [ ! -f "/home/container/__var_exiled_installed" ]
+EXAMPLE_PATH="/home/container/ExiledExample"
 
 reinstallExiled(){
     echo "Reinstalling EXILED..."
@@ -52,3 +24,19 @@ reinstallServer(){
     ./steamcmd.sh +force_install_dir ./scp_server +login anonymous +app_update 996560 validate $EX +quit
     echo "SCPSL Successfully installed"
 }
+
+if [ $reinstall != 0 ] && [ $modded == 1 ]; then 
+    reinstallExiled
+fi
+if [ $modded == 0 ]; then
+    reinstallServer
+fi
+if [ ! -f $CONFIG_PATH/EXILED ]; then
+    cp -r $EXAMPLE_PATH/. $CONFIG_PATH/
+fi;
+
+# Startup server
+export DOTNET_BUNDLE_EXTRACT_BASE_DIR="temp"
+echo "Stating server at $server_port port.."
+cd $SERVER_PATH
+./LocalAdmin $server_port
--- exiled-preinstallation.sh
+++ exiled-preinstallation.sh
@@ -1,10 +1,12 @@
-SERVER_PATH="/home/container/scp_server/"
-CONFIG_PATH="/home/container/.config/"
+SERVER_PATH="/home/container/scp_server"
+EXAMPLE_PATH="/home/container/ExiledExample"
 
 URL="https://github.com/Exiled-Team/EXILED/releases/download/5.2.1/Exiled.Installer-Linux"
 
 echo "Installing EXILED..."
+mkdir $EXAMPLE_PATH
 wget $URL
-chmod +x Exiled.Installer-Linux
-./Exiled.Installer-Linux --appdata $CONFIG_PATH -p $SERVER_PATH $EXTRA
+chmod +x Exiled.Installer-Linux $EXAMPLE_PATH
+./Exiled.Installer-Linux --appdata $EXAMPLE_PATH -p $SERVER_PATH $EXTRA
 rm ./Exiled.Installer-Linux 
+echo "EXILED successfully installed!"
